faad42c982449652a8d29943be5fb617db8920f8
Hichem <148470835+Hichem212@users.noreply.github.com>
2024-02-29 20:28:01 +0100

    création de la méthode trouverHabitant

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,13 +2,50 @@ package personnages;
 
 public class Village {
 	private String nom;
-		
-	public Village(String nom) {
+	private Chef chef;
+	private int nbVillageois;
+	private Gaulois [] villageois; 
+	int nbVillageoisMaximum;
+	
+	public Village(String nom ,int nbVillageoisMaximum) {
 		this.nom = nom;
-	}
+		this.nbVillageois = 0;
+		this.villageois = new Gaulois[nbVillageoisMaximum]; 
 		
+	}
+	
+		
+	
+	public Chef getChef() {
+		return chef;
+	}
+
+
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
+
+
 	public String getNom() {
 		return nom;
 	}
 	
+	public void ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois < villageois.length) {
+            villageois[nbVillageois] = gaulois;
+            nbVillageois++;
+		}
+    }
+	public Gaulois trouberHabitant(int numero) {
+		return villageois[numero];
+		
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductible",30);
+		
+	
+	}
 }
