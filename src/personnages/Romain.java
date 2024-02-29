1a52d6610014420d39f26349a037089ed566bc11
Hichem <148470835+Hichem212@users.noreply.github.com>
2024-02-29 21:32:12 +0100

    Invariant pre et post condition

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -27,12 +27,26 @@ public class Romain {
 	}
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup ;
+		
+		if (force <= 0) {
+	        throw new IllegalArgumentException("La force d'un Romain doit être positive");
+		}
+	    int forceInitiale = force;
 		if (force > 0 ) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne ...") ; 
-
+			
 		}
+		if (force >= forceInitiale) {
+	        throw new IllegalStateException("La force d'un Romain n'a pas diminué après avoir reçu un coup");
+		}
+		
+	}
+	public static void main(String[] args) {
+		Romain romain = new Romain("LER",-2);
+		Romain romain = new Romain("Minus",6);
+		
 		
 	}
 }
